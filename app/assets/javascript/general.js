d0f27296ce2e107b02f7e36bb5738b544f95aec4
Elton Santos <eltonaxl@hotmail.com>
2021-08-10 00:40:00 -0300

    More exercises finished

---
+++ app/assets/javascript/general.js
@@ -0,0 +1,8 @@
+// click on logo, get welcome message
+document.addEventListener("DOMContentLoaded", function() {
+  var logo = document.getElementById('logo');
+  logo.addEventListener('click', function(event) {
+    event.preventDefault();
+    alert('Welcome!');
+  });
+});
